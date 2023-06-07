99d4aadb5cca728ca148be9e725919bdbfd1d172
Ademola006 <125704171+Ademola006@users.noreply.github.com>
2023-06-07 13:52:25 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { UserInput } from './component/userInput'  //importing the UserInput from the component
+import { UserOutput } from './component/userOutput' //importing the UserOnput from the component
+
+
+const App =() => {
+  return (
+    <div className='container'> 
+      <UserInput/>        
+      <UserOutput/>
+      <UserOutput/>
+      <UserOutput/>
+      <UserOutput/>
+      <UserOutput/>
+      <UserOutput/>
+      <UserOutput/>
+      <UserOutput/>
+      
+
+     
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/userOutput.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export const UserOutput = () =>{
+  return (
+    <div>
+        <p>My name is Ademola and i am a participant of the Sail program. I have met some wonderful peopel in the program.</p>
+        <p>Today is a beautiful day and i'm glad to be alive.</p>
+
+    </div>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
